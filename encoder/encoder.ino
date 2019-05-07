00ac727c2acb4ffa92a00261562e628ef5935962
javier <javiermdg998@gmail.com>
2019-05-07 20:00:27 +0200

    sensor

---
+++ encoder/encoder.ino
@@ -0,0 +1,27 @@
+/* Counter Library - Basic Example
+ * https://github.com/joruiz/Counter
+ *
+ * This example code is in the public domain.
+ */
+ 
+#include <Counter.h>
+
+Counter myCounter(7);
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("Basic Counter Test:");
+}
+
+
+long oldValue  = -999;
+
+void loop() {
+
+  long newValue = myCounter.read();
+  if (newValue != oldValue) {
+    oldValue = newValue;
+    Serial.println(newValue);
+  }
+}
